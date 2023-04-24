e9004a728e750db7f9d91e5e1461b4e2aed95881
Zainab Aliyu <zainabaliyu2705@gmail.com>
2023-04-24 02:29:00 -0700

    comments added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if(n>0){
-		printf("%d: is positive \n", n);
-	} else if(n == 0){
-		printf("%d: is zero\n", n);
-	}else{
-		printf("%d: is negative\n", n);
+
+/* checks if a number is posite or negative*/
+
+	if(n > 0) {
+		printf("%d is positive \n", n);
+	} else if(n == 0) {
+		printf("%d is zero\n", n);
+	} else {
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
