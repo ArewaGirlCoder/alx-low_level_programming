21b9661288550640c15ab029094806617939dd9c
Zainab Aliyu <zainabaliyu2705@gmail.com>
2023-04-28 17:11:41 -0700

    main.h and readme file added

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*File : main.h
+ *
+ * Desc.: contains all your functions and the prototype of the function _putchar
+ */
+
+
+
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
